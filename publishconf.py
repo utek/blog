8d88596c16dfc3d9ef2f87bc76b8b6a828cb240d
Łukasz Bołdys <lukasz@boldys.com>
2026-02-15 11:31:32 +0000

    Pre-commit, fix line ending

--- publishconf.py
+++ publishconf.py
@@ -7,14 +7,15 @@ from __future__ import unicode_literals
 
 import os
 import sys
-sys.path.append(os.curdir)
-from pelicanconf import *
 
-SITEURL = 'http://dev.utek.pl'
+sys.path.append(os.curdir)
+from pelicanconf import *  # noqa: F403
+
+SITEURL = "http://dev.utek.pl"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/%s.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
